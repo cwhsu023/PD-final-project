0e4b7dc56033db63ee6ab0cb4a3e0b9c9bd8d5a4
yehgrace <75880658+yehgrace@users.noreply.github.com>
2022-01-02 23:21:21 +0800

    Add files via upload
    首頁兩個按鈕連到排行榜及遊戲頁面

--- test.cpp
+++ test.cpp
@@ -1,44 +1,104 @@
 #include <SFML/Graphics.hpp>
-#include<iostream>
-using namespace std;
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <iostream>
 
-int main()
-{
-sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-sf::CircleShape shape(100.f);
-shape.setFillColor(sf::Color::Blue);
 
-while (window.isOpen())
+bool isSpriteHover(sf::FloatRect sprite, sf::Vector2f mp) 
 {
-    sf::Event event;
-    while (window.pollEvent(event))
+    if (sprite.contains(mp))
     {
-        if (event.type == sf::Event::Closed)
-        {
-            cout << "close" << endl;
-            window.close();
-        }
-        if (isKeyPressed(Keyboard::A)) {
-            changeX = -2;
-        } else if (isKeyPressed(Keyboard::D) {
-            changeX = 2;
-        } else {
-            changeX = 0;
-        } if (isKeyPressed(Keyboard::W) {
-            changeY = -2;
-        } else if (isKeyPressed(Keyboard::S)) {
-            changeY = 2;
-        } else {
-            changeY = 0;
-        }
-
+        return true;
     }
-    
-
-    window.clear();
-    window.draw(shape);
-    window.display();
+    return false;
 }
 
+
+int main()
+{       
+
+    sf::RenderWindow window, window2, window3;
+            window.create(sf::VideoMode(3000, 2500),"My first Visual Studio window!");
+    
+    sf::Texture texture;
+    if(!texture.loadFromFile("button1.png"))
+    {
+        return 1;
+    }
+    sf::Sprite sprite1;
+    sprite1.setTexture(texture);
+
+    sf::Texture texture2;
+    if(!texture2.loadFromFile("champion_board.png"))
+    {
+        return 1;
+    }
+    sf::Sprite sprite2;
+    sprite2.setTexture(texture2);
+
+    sf::Vector2f mp;
+    mp.x = sf::Mouse::getPosition(window).x;
+    mp.y = sf::Mouse::getPosition(window).y;
+
+
+
+        while(window.isOpen())
+        {
+            sf::Event event;
+                
+                while(window.pollEvent(event))
+                {
+                    if(event.type == sf::Event::Closed)
+                        window.close();                 
+            
+                    if(isSpriteHover(sprite1.getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
+                    {
+                        if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
+                        {
+                                window.create(sf::VideoMode(1800, 1600),"Game!");
+                                while(window.isOpen())
+                                {
+                                    sf::Event event;
+                                    
+                                    while(window.pollEvent(event))
+                                    {
+                                        if(event.type == sf::Event::Closed)
+                                            window.close(); 
+                                    }
+                                }
+                        }
+                    }
+                    if(isSpriteHover(sprite2.getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
+                    {
+                        if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
+                        {
+                                window.create(sf::VideoMode(1800, 1600),"Rank!");
+                                while(window.isOpen())
+                                {
+                                    sf::Event event;
+                                    
+                                    while(window.pollEvent(event))
+                                    {
+                                        if(event.type == sf::Event::Closed)
+                                            window.close(); 
+                                    }
+                                }
+                        }
+                    }
+                }
+            window.clear(sf::Color::Black);
+
+            sprite1.setPosition(sf::Vector2f(50, 300));
+            
+            window.draw(sprite1);
+
+            sprite2.setPosition(sf::Vector2f(1500, 300));
+            
+            window.draw(sprite2);
+            
+            window.display();
+
+}
+        
 return 0;
 }
