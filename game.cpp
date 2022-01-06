a0f9de6128d545f0313ad246c4b481e26f107a5d
cwhsu023 <75806504+cwhsu023@users.noreply.github.com>
2022-01-06 15:58:52 +0800

    Add files via upload

---
+++ game.cpp
@@ -0,0 +1,104 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <iostream>
+#include <string>
+#include <time.h>
+
+class Game
+{
+public:
+    int k;
+    sf::RenderWindow window;
+    sf::Texture t1, t2, t3;
+    sf::Sprite s1, s2, s3;
+    int t1PosX, t3PosX;
+    time_t startTime, endTime;
+    Game();
+    void refresh();
+    void openWindow(); 
+};
+Game::Game()
+{
+    this->k = 1;
+    this->window.create(sf::VideoMode(3000, 2500),"Game!!!");
+    openWindow();
+}
+void Game::openWindow()
+{
+    t1.loadFromFile("background.png");
+    s1.setTexture(t1);
+    t1PosX = 0;
+    s1.setPosition(sf::Vector2f(t1PosX, 300));
+    t3.loadFromFile("background.png");
+    s3.setTexture(t3);
+    t3PosX = t3.getSize().x;
+    s3.setPosition(sf::Vector2f(t3PosX,400));
+    std::string fileName = "bike" + std::to_string(k) + ".png";
+    t2.loadFromFile(fileName);
+    s2.setTexture(t2);
+    s2.setPosition(sf::Vector2f(100,400));
+    
+    startTime = time(0);
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                double time_dif = difftime( time(0), startTime);
+                std::cout << "You've played " << time_dif << " seconds" << std::endl;  // 如果要計時要換一個好一點的計時方法
+                window.close();
+            }
+            if(sf::Event::KeyPressed)
+            {
+                switch(event.key.code)
+                {
+                    case sf::Keyboard::Right:
+                    {
+                        s2.move(20,0);
+                        break;
+                    }
+                    case sf::Keyboard::Left:
+                    {
+                        s2.move(-20,0);
+                        break;
+                    }
+                    case sf::Keyboard::Up:
+                    {
+                        s2.move(0,-20);
+                        break;
+                    }
+                    case sf::Keyboard::Down:
+                    {
+                        s2.move(0,20);
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }
+        }
+        refresh();
+    }
+}
+void Game::refresh()
+{
+    this->window.clear(sf::Color::Black);
+    s1.setPosition(sf::Vector2f(t1PosX, 300));
+    s3.setPosition(sf::Vector2f(t3PosX, 300));
+    this->window.draw(this->s1);
+    // std::string fileName = "bike" + std::to_string(k) + ".png";
+    // t2.loadFromFile(fileName);
+    // s2.setTexture(t2);
+    // s2.setPosition(sf::Vector2f(100,400));
+    this->window.draw(this->s3);
+    this->window.draw(this->s2);
+    this->window.display();
+    // k == 4 ? k = 1 : k++;
+    t1PosX == -(t1.getSize().x) ? t1PosX = t1.getSize().x : t1PosX--;
+    t3PosX == -(t3.getSize().x) ? t3PosX = t3.getSize().x : t3PosX--;
+
+}
+
--- main.cpp
+++ main.cpp
@@ -2,107 +2,13 @@
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 #include <iostream>
-#include<string>
-#include "page.cpp"
+#include <string>
+#include "login.cpp"
+
+
 
 int main()
-{    
-    Page login(3000, 2500, "Game!!");
-    login.addImage(50, 300, "button1.png");
-    login.addImage(1500, 300, "champion_board.png");
-    sf::Vector2f mp;
-    mp.x = sf::Mouse::getPosition(login.window).x;
-    mp.y = sf::Mouse::getPosition(login.window).y;
-
-    while(login.Open())
-    {
-        sf::Event event;
-            
-        while(login.window.pollEvent(event))
-        {
-            if(event.type == sf::Event::Closed)
-                login.window.close();                 
-    
-            if(isSpriteHover(login.s[0].getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
-            {
-                if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
-                {
-                        sf::RenderWindow window2;
-                        window2.create(sf::VideoMode(1800, 1600),"Let's Gooooooo!!!!!");
-                        sf::Texture texture3;
-                        if(!texture3.loadFromFile("topview_car.png"))
-                        {
-                            return 1;
-                        }
-                        sf::Sprite sprite3;
-                        sprite3.setTexture(texture3);
-                        int sprite3x = 50, sprite3y = 300;
-                        sprite3.setPosition(sf::Vector2f(sprite3x, sprite3y));
-                        while(window2.isOpen())
-                        {
-                            sf::Event event;
-
-                            while(window2.pollEvent(event))
-                            {
-                                if(event.type == sf::Event::Closed)
-                                    window2.close(); 
-                                if(sf::Event::KeyPressed)
-                                {
-                                    switch(event.key.code)
-                                    {
-                                        case sf::Keyboard::Right:
-                                        {
-                                            sprite3.move(20,0);
-                                            break;
-                                        }
-                                        case sf::Keyboard::Left:
-                                        {
-                                            sprite3.move(-20,0);
-                                            break;
-                                        }
-                                        case sf::Keyboard::Up:
-                                        {
-                                            sprite3.move(0,-20);
-                                            break;
-                                        }
-                                        case sf::Keyboard::Down:
-                                        {
-                                            sprite3.move(0,20);
-                                            break;
-                                        }
-                                        default:
-                                            break;
-                                    }
-                                }
-                            }
-                            
-                            window2.clear(sf::Color::Black);
-                            window2.draw(sprite3);
-                            window2.display();
-                        }
-                }
-            }
-            // if(isSpriteHover(sprite2.getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
-            // {
-            //     if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
-            //     {
-            //             window.create(sf::VideoMode(1800, 1600),"Rank!");
-            //             while(window.isOpen())
-            //             {
-            //                 sf::Event event;
-                            
-            //                 while(window.pollEvent(event))
-            //                 {
-            //                     if(event.type == sf::Event::Closed)
-            //                         window.close(); 
-            //                 }
-            //             }
-            //     }
-            // }
-        }
-        login.refresh();
-
-}
-        
-return 0;
+{
+    Login login;
+    return 0;
 }
