79b297bcce4fc9303153f026698bf54571f4b654
cwhsu023 <75806504+cwhsu023@users.noreply.github.com>
2022-01-09 04:45:17 +0800

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -4,32 +4,27 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include "player.h"
 
-class Player
-{
-public:
-    friend class Game;
-    Player();
-};
-Player::Player()
-{
-
-}
 class Game
 {
 public:
+    int c;
     int k;
     sf::RenderWindow window;
-    sf::Texture t1, t2, t3, ot1;
-    sf::Sprite s1, s2, s3, os1;
-    int t1PosX, t3PosX;
+    sf::Texture t1, t3, ot1;
+    sf::Sprite s1, s3, os1;
+    std::vector<sf::Sprite> obstacles;
+    int t1PosX, t3PosX, dovePos;
     time_t startTime, endTime;
     Game();
-    void refresh();
+    void refresh(Player &player);
     void openWindow(); 
+    void collision(Player &player, sf::Sprite &obs);
 };
 Game::Game()
 {
+    this->c = 0;
     this->k = 49;
     this->window.create(sf::VideoMode(3000, 1200),"Game!!!");
     openWindow();
@@ -46,79 +41,101 @@ void Game::openWindow()
     s3.setPosition(sf::Vector2f(t3PosX,0));
     Player player;
     std::string fileName = "Newbike" + std::to_string(k/10) + ".png";
-    t2.loadFromFile(fileName);
-    s2.setTexture(t2);
-    s2.setPosition(sf::Vector2f(100,400));
-    // ot1.loadFromFile("dove.png", sf::IntRect(1285,460,2285,1200));
-    ot1.loadFromFile("dove.png");
+    player.t2.loadFromFile(fileName, sf::IntRect(30,370,450,770));
+    player.s2.setTexture(player.t2);
+    player.s2.setPosition(sf::Vector2f(100,400));
+    ot1.loadFromFile("dove.png", sf::IntRect(1285,460,2285,1200));
+    // ot1.loadFromFile("dove.png");
     os1.setTexture(ot1);
     os1.setScale(sf::Vector2f(0.3f, 0.3f));
-    os1.setPosition(sf::Vector2f(700, 700));
-    std::cout << ot1.getSize().x << std::endl;
-    std::cout << ot1.getSize().y << std::endl;
-    std::cout << t1.getSize().x << "," << t1.getSize().y << std::endl;
+    dovePos = 700;
+    os1.setPosition(sf::Vector2f(dovePos, 700));
+    obstacles.push_back(os1);
+
+    // std::cout << os1.getPosition().x << std::endl;
+    // std::cout << os1.getGlobalBounds().width << std::endl;
+
+    float dt;
+    sf::Clock dt_clock;
 
     
-    startTime = time(0);
+    sf::Clock timer;
     while(window.isOpen())
     {
+        dt = dt_clock.restart().asMilliseconds();
         sf::Event event;
         while(window.pollEvent(event))
         {
             if(event.type == sf::Event::Closed)
             {
-                double time_dif = difftime( time(0), startTime);
-                std::cout << "You've played " << time_dif << " seconds" << std::endl;  // 如果要計時要換一個好一點的計時方法
+                sf::Time elapsed = timer.getElapsedTime();
+                std::cout << elapsed.asSeconds() << std::endl;
                 window.close();
             }
+            player.velocity.x = 0.f;
+            player.velocity.y = 0.f;
             if(sf::Event::KeyPressed)
             {
                 switch(event.key.code)
                 {
                     case sf::Keyboard::Right:
                     {
-                        s2.move(20,0);
+                        player.velocity.x += player.moveSpeed * dt;
                         break;
                     }
                     case sf::Keyboard::Left:
                     {
-                        s2.move(-20,0);
+                        player.velocity.x += -player.moveSpeed * dt;
                         break;
                     }
                     case sf::Keyboard::Up:
                     {
-                        s2.move(0,-20);
+                        player.velocity.y += -player.moveSpeed * dt;
                         break;
                     }
                     case sf::Keyboard::Down:
                     {
-                        s2.move(0,20);
+                        player.velocity.y += player.moveSpeed * dt;
                         break;
                     }
                     default:
                         break;
                 }
+                player.s2.move(player.velocity);
             }
         }
-        refresh();
+        collision(player, os1);
+        refresh(player);
     }
 }
-void Game::refresh()
+void Game::refresh(Player &player)
 {
     this->window.clear(sf::Color::Black);
     s1.setPosition(sf::Vector2f(t1PosX, 0));
     s3.setPosition(sf::Vector2f(t3PosX, 0));
-    this->window.draw(this->s1);
+    os1.setPosition(sf::Vector2f(dovePos, 700));
     std::string fileName = "Newbike" + std::to_string(k/10) + ".png";
-    t2.loadFromFile(fileName);
-    s2.setTexture(t2);
+    player.t2.loadFromFile(fileName, sf::IntRect(30,370,450,770));
+    player.s2.setTexture(player.t2);
+    player.checkPosition();
+    this->window.draw(this->s1);
     this->window.draw(this->s3);
-    this->window.draw(s2);
+    this->window.draw(player.s2);
     this->window.draw(this->os1);
     this->window.display();
     k == 10 ? k = 49 : k--;
     t1PosX == -(t1.getSize().x) ? t1PosX = t1.getSize().x : t1PosX--;
     t3PosX == -(t3.getSize().x) ? t3PosX = t3.getSize().x : t3PosX--;
-
+    dovePos--;
 }
-
+void Game::collision(Player &player, sf::Sprite &obs)
+{
+    c++;
+    sf::FloatRect playerBounds = player.s2.getGlobalBounds();
+    sf::FloatRect obsBounds = obs.getGlobalBounds();
+    if( playerBounds.intersects(obsBounds))
+    {
+        std::cout << "撞到鴿子了"  << c << std::endl;
+        // std::cout << playerBounds.left << "," << obsBounds.left << std::endl;
+    }
+}
---
+++ player.h
@@ -0,0 +1,29 @@
+class Player
+{
+public:
+    sf::Texture t2;
+    sf::Sprite s2;
+    int life;
+    float moveSpeed;
+    sf::Vector2f velocity;
+    friend class Game;
+    Player();
+    void checkPosition();
+};
+Player::Player():moveSpeed(1.f)
+{
+    life = 1;
+}
+void Player::checkPosition()
+{
+    if(s2.getGlobalBounds().left < 0)
+        s2.setPosition(0, s2.getPosition().y);
+    if(s2.getGlobalBounds().left + s2.getGlobalBounds().width > 2950)
+        s2.setPosition(2950 - s2.getGlobalBounds().width, s2.getPosition().y);
+    if(s2.getPosition().y < 275)
+        s2.setPosition(s2.getPosition().x, 275);
+    if(s2.getGlobalBounds().top + s2.getGlobalBounds().height > 1250)
+        s2.setPosition(s2.getPosition().x, 1250 - s2.getGlobalBounds().height);
+    // std::cout << s2.getPosition().x << std::endl;
+    // std::cout << s2.getPosition().y << std::endl;
+}
