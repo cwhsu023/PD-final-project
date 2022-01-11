0168fe310b3d049d0309d8b6d9ce0c48e6b09531
franco-1012 <75825931+franco-1012@users.noreply.github.com>
2022-01-11 20:48:01 +0800

    Add files via upload

--- bike.h
+++ bike.h
@@ -11,6 +11,7 @@ public:
     void bikeAnimationSpeed(float& speedControler);
     void bikeAccelerating();
     void bikeDecelerating();
+    void testbikeAccelerating();
     //bike spped : smooth accelerating 
 };
 
@@ -23,6 +24,21 @@ float Bike::bikeSpeed()
 {
     return speedControl ;
 }
+void Bike::testbikeAccelerating()
+{
+    if(this -> speedControl < 0)
+        this -> speedControl = 0;
+    else if(this -> speedControl >=0 && this -> speedControl <= 0.05)
+        this -> speedControl += 0.003 ;
+    else if(this -> speedControl > 0.05 && this -> speedControl <= 0.08) 
+        this -> speedControl += 0.004 ;
+    else if(this -> speedControl > 0.08 && this -> speedControl <= 0.15)
+        this -> speedControl += 0.002 ;
+    else if(this -> speedControl >= 0.15 && this -> speedControl <= 0.2)
+        this -> speedControl += 0.01 ;
+    else if(this -> speedControl > 0.2 )
+        this -> speedControl = 0.2 ;
+}
 void Bike::bikeAccelerating()
 {
     if(this -> speedControl < 0)
@@ -33,14 +49,6 @@ void Bike::bikeAccelerating()
         this -> speedControl += 0.0017 ;
     else if(this -> speedControl > 0.08 && this -> speedControl <= 0.15)
         this -> speedControl += 0.0030 ;
-    // else if(this -> speedControl > 0.035 && this -> speedControl <= 0.04)
-    //     this -> speedControl += 0.0033 ;
-    // else if(this -> speedControl > 0.04 && this -> speedControl <= 0.06)
-    //     this -> speedControl += 0.0025 ;
-    // else if(this -> speedControl > 0.06 && this -> speedControl <= 0.08)
-    //     this -> speedControl += 0.0020 ;
-    // else if(this -> speedControl > 0.1 && this -> speedControl <= 0.15)
-    //     this -> speedControl += 0.0015 ;
     else if(this -> speedControl >= 0.15 && this -> speedControl <= 0.2)
         this -> speedControl += 0.01 ;
     else if(this -> speedControl > 0.2 )
