9f93299b528490a1ac602c3309d295a736e64053
gary900130 <76398787+gary900130@users.noreply.github.com>
2022-01-13 20:47:26 +0800

    Add files via upload

--- codes/game.cpp
+++ codes/game.cpp
@@ -5,18 +5,33 @@
 #include <string>
 #include <time.h>
 #include <stdlib.h>
+#include <fstream>
 #include "player.h"
-#include "obstacles.h"
+// #include "obstacles.h"
 #include "bike.h"
-std::vector<Obstacles> allObstacle;
+#include "randomDove.h"
+// std::vector<Obstacles> allObstacle;
+bool isSpriteHover(sf::FloatRect sprite, sf::Vector2f mp) 
+{
+    if (sprite.contains(mp))
+    {
+        return true;
+    }
+    return false;
+}
 class Game
 {
 public:
     int c, totalDist;
     bool inLibrary;
     sf::RenderWindow window;
-    sf::Texture t1, gOverT, t_heart1, t_heart2, t_heart3;
-    sf::Sprite s1, gOverS, s_heart1, s_heart2, s_heart3;
+    sf::Texture t1, gOverT, t_heart1, t_heart2, t_heart3, t_libraryFront;
+    sf::Sprite s1, gOverS, s_heart1, s_heart2, s_heart3, s_libraryFront;
+    sf::Font font;
+    sf::Text text;
+    sf::RectangleShape r1, r2;
+    sf::Vector2f mp;
+    sf::SoundBuffer bgmBuffer, winBuffer, loseBuffer;
     int t1PosX, t3PosX, dovePos;
     time_t startTime, endTime;
     Game();
@@ -24,6 +39,8 @@ public:
     void openWindow();
     void heart(int playerLife);
     friend class Obstacles;
+    void victory(Player &player , int victoryAnimateControl);
+    // void getDove();
 };
 Game::Game()
 {
@@ -70,21 +87,42 @@ void Game::heart(int playerLife)
 }
 void Game::openWindow()
 {
+	//playing BGM
+	bgmBuffer.loadFromFile("resources/gameBGM.ogg");
+	sf::Sound bgm(bgmBuffer);
+	bgm.play();
+	bgm.setLoop(true);
+	
+	//setting win & fail music
+	bgmBuffer.loadFromFile("resources/win.ogg");
+	sf::Sound win(winBuffer);
+	bgmBuffer.loadFromFile("resources/lose.ogg");
+	sf::Sound lose(loseBuffer);
+	
+	// mouse position
+    mp.x = sf::Mouse::getPosition(this->window).x;
+    mp.y = sf::Mouse::getPosition(this->window).y;
+
     //setting backgound
     t1.loadFromFile("resources/NEWroad.png");
     s1.setTexture(t1);
     t1PosX = 0;
     s1.setPosition(sf::Vector2f(t1PosX, 0));
+    t_libraryFront.loadFromFile("resources/45libraryFront.png");
+    s_libraryFront.setTexture(t_libraryFront);
+    // s_libraryFront.setPosition(sf::Vector2f(totalDist * this->s1.getGlobalBounds().width, 0));
     
     //setting bike 
     Player player;
     
-
     // obstacles
     dovePos = 700;
+    // srand(time(NULL));
     srand(time(NULL));
-    // Obstacles dove1(500 + (rand() % 10000), 600 + (rand() % 600), -(rand() % 10) - 5, 0);
-    // allObstacle.push_back(dove1);
+    getRandomDove getRandomDove;
+
+    // text setting
+    font.loadFromFile("resources/gen.ttf");
 
 
     // background moving setting 
@@ -109,14 +147,12 @@ void Game::openWindow()
     sf::Clock timer;
 
     int bikeAnimateControl = 19 ;
-    //跑了幾圈while 
-    int distane =0 ;
+    int victoryAnimateControl = 15 ;
     //速度
     // float speedControler = 0.01 ;
     Bike bike;
     while(window.isOpen())
     {
-        
         dt = dt_clock.restart().asMilliseconds();
         sf::Event event;
         while(window.pollEvent(event))
@@ -128,6 +164,24 @@ void Game::openWindow()
                 allObstacle.erase(allObstacle.begin(), allObstacle.begin()+allObstacle.size());
                 window.close();
             }
+            if(isSpriteHover(r1.getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
+                {
+                    if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        allObstacle.erase(allObstacle.begin(), allObstacle.begin()+allObstacle.size());
+                        bgm.stop();
+                        window.close();
+                        Game game;
+                    }
+                }
+            if(isSpriteHover(r2.getGlobalBounds(), sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) == true)
+                {
+                    if(event.type == sf::Event::MouseButtonReleased &&  event.mouseButton.button == sf::Mouse::Left)
+                    {
+                        allObstacle.erase(allObstacle.begin(), allObstacle.begin()+allObstacle.size());
+                        window.close();
+                    }
+                }
             player.velocity.x = 0.f;
             player.velocity.y = 0.f;
             if(sf::Event::KeyPressed)
@@ -163,9 +217,46 @@ void Game::openWindow()
                 player.rectangle.move(player.velocity);
             }
         }
+        if(offset > totalDist && !inLibrary )
+        {
+            // window.clear();
+            t1.loadFromFile("resources/45libraryBack.png");
+            s1.setTexture(t1);
+            inLibrary = true ;
+        }
+        if(offset > totalDist + 0.4)
+        {
+            window.clear();
+            // inLibrary = true;
+            float speedControler = bike.bikeSpeed() ;
+            float currentSpeed = clock.restart().asSeconds() * speedControler;
+            offset += currentSpeed;
+            if(inLibrary && (offset - totalDist) * this->s1.getGlobalBounds().width > this->s1.getGlobalBounds().width - 3000)
+                offset = totalDist + (static_cast<float>(this->s1.getGlobalBounds().width - 3000) / this->s1.getGlobalBounds().width);
+            parallaxShader.setUniform("offset", offset = offset);
+            window.draw(s1, &parallaxShader);
+            victory(player ,victoryAnimateControl);
+            player.velocity.x = 10.0f;
+            player.velocity.y = 0.0f;
+            player.s_victory.move(player.velocity);
+            // if((offset - totalDist) * this->s1.getGlobalBounds().width > 2000)
+            //     std::cout << "yep "<< std::endl ;
+            // std::cout << s1.getGlobalBounds().left << std::endl;
+            float ppos = -((offset - totalDist) * s1.getGlobalBounds().width);
+            s_libraryFront.setPosition(sf::Vector2f(ppos, 0)); //sf::Vector2f(totalDist * this->s1.getGlobalBounds().width
+            window.draw(s_libraryFront) ;
+    
+            victoryAnimateControl--;
+            if(victoryAnimateControl < 4)
+            {
+                victoryAnimateControl = 15 ;
+            }
+            window.display();
+            continue ;
+        }
         for(int i = 0; i < allObstacle.size(); i++)
         {
-            
+            // chceck collision
             bool hit = false;
             hit = allObstacle[i].collision(player);
             if(hit)
@@ -181,6 +272,8 @@ void Game::openWindow()
             //first bakgound , second bike , third dove or others ! display~
             //background drawing
             window.clear();
+            if(!player.alive)
+                bike.speedControl = 0;
             float speedControler = bike.bikeSpeed() ;
             float currentSpeed = clock.restart().asSeconds() * speedControler;
             offset += currentSpeed;
@@ -225,23 +318,66 @@ void Game::openWindow()
             }
             player.hit = false;
         }
-        if(player.life == 0)
+        if(player.life <= 0 && player.alive)
         {
             // gameover setting
             sf::Time elapsed = timer.getElapsedTime();
-            gOverT.loadFromFile("resources/topView_car.png");
+            lose.play();
+            gOverT.loadFromFile("resources/gameover.png");
             gOverS.setTexture(gOverT);
-            gOverS.setPosition(sf::Vector2f(700, 700));
+            gOverS.setPosition(sf::Vector2f(1150, 400));
+            gOverS.setScale(sf::Vector2f(1.2f,1.2f));
+            r1.setSize(sf::Vector2f(110, 25));
+            r1.setFillColor(sf::Color::Transparent);
+            // r1.setOutlineColor(sf::Color::Red);
+            // r1.setOutlineThickness(5);
+            r1.setPosition(1322, 610);
+            r2.setSize(sf::Vector2f(110, 25));
+            r2.setFillColor(sf::Color::Transparent);
+            // r2.setOutlineColor(sf::Color::Red);
+            // r2.setOutlineThickness(5);
+            r2.setPosition(1322, 675);
             std::cout << "Game over. You've survived for " << elapsed.asSeconds() << " seconds" << std::endl;
             allObstacle.erase(allObstacle.begin(), allObstacle.begin()+allObstacle.size());
+            player.alive = false;
+
+            text.setString(std::to_string(elapsed.asSeconds()));
+            text.setFont(font);
+            text.setCharacterSize(20); // exprimée en pixels, pas en points !
+            text.setFillColor(sf::Color::Black);
+            text.setPosition(330 , 400);
+
+            float score = elapsed.asSeconds();
+            std::ifstream inFile("resources/output.txt");
+            std::ofstream outFile("resources/output.txt");
+            std::string place;
+            float file_score;
+            float all_score[5] = {0};
+            std::cout << score << std::endl;
+            if(inFile && outFile)  //讀得到檔案
+            {
+                int i = 0;
+                while(inFile >> file_score)
+                {
+                    if(score < file_score)
+                    {
+                        float temp_score = file_score;
+                        file_score = score;
+                        score = temp_score;
+                    }
+                    all_score[i] = file_score;
+                    i++;
+                    std::cout << i << "," << file_score << "," << score << std::endl;
+                }
+                outFile << "No.1 " << all_score[0] << std::endl;
+                outFile << "No.2 " << all_score[1] << std::endl;
+                outFile << "No.3 " << all_score[2] << std::endl;
+                outFile << "No.4 " << all_score[3] << std::endl;
+                outFile << "No.5 " << all_score[4] << std::endl;
+            }
+            inFile.close();
+            outFile.close();
             // window.close();
-        }
-        if(offset > totalDist && !inLibrary )
-        {
-            // ending pic
-            t1.loadFromFile("resources/library.png");
-            s1.setTexture(t1);
-            inLibrary = true;
         }
 
         
@@ -249,6 +385,14 @@ void Game::openWindow()
 }
 void Game::refresh(Player &player, int bikeAnimateControl, float currentSpeed)
 {
+    // game over
+    if(!player.alive)
+    {
+        this->window.draw(gOverS);
+        this->window.draw(text);
+        this->window.draw(r1);
+        this->window.draw(r2);
+    }
     // bike animation 
     std::string fileName = "resources/Newbike" + std::to_string(bikeAnimateControl/4) + ".png";
     player.t2.loadFromFile(fileName, sf::IntRect(30,370,450,740));
@@ -283,8 +427,16 @@ void Game::refresh(Player &player, int bikeAnimateControl, float currentSpeed)
             this->window.draw(obs.obsSprite);
             this->window.draw(obs.circle);
         }
-    }
-    
-    // game over
-    this->window.draw(gOverS);
+    }    
+}
+void Game::victory(Player &player , int victoryAnimateControl)
+{
+    //bike victory animate
+    win.play();
+    std::cout <<"victory" << std::endl ;
+    std::string fileName3 = "resources/NewYahoo" + std::to_string(victoryAnimateControl/4) + ".png";
+    player.t_victory.loadFromFile(fileName3, sf::IntRect(600,300,450,740)); //, sf::IntRect(30,370,450,740)
+    player.s_victory.setTexture(player.t_victory);
+    this -> window.draw(player.s_victory);
+
 }
